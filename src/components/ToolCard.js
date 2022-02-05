bdce784bb9b716ca5d94593762227ea4723a9d78
sb-haz <sb_haz@icloud.com>
2022-02-05 05:24:55 +0000

    improve design css

--- src/components/ToolCard.js
+++ src/components/ToolCard.js
@@ -2,9 +2,10 @@ const ToolCard = ({ title, body }) => {
     return (
         <div className="tool-card">
             <img src="https://via.placeholder.com/200" alt="" />
+            <p>NEW</p>
             <h2>{title}</h2>
             <p>{body}</p>
-            <button>Use</button>
+            <a href="#" className="gradient-btn">Use</a>
         </div>
     );
 };
