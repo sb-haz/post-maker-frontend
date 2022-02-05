017017636b5bced399746077ba4c1a3e5683aa9b
sb-haz <sb_haz@icloud.com>
2022-02-05 22:29:47 +0000

    fix border issue

--- src/components/DemoVideo.js
+++ src/components/DemoVideo.js
@@ -1,12 +1,12 @@
 const DemoVideo = () => {
     return (
         <div className="hero-video">
-            <video class="shadow-xl" width="350" height="350" playsinline controls autoplay muted loop>
+            <video width="350" height="350" autoplay /*muted*/ playsinline controls loop >
                 <source src="/examples/video_maker.mp4"
-                    type="video/mp4"/>
+                    type="video/mp4" />
             </video>
         </div>
-        
+
     );
 };
 
