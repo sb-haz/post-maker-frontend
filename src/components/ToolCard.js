51200adba499f6ab36765a9cd449f575e22ace04
sb-haz <sb_haz@icloud.com>
2022-02-05 05:50:47 +0000

    improve card design

--- src/components/ToolCard.js
+++ src/components/ToolCard.js
@@ -2,8 +2,9 @@ const ToolCard = ({ title, body }) => {
     return (
         <div className="tool-card">
             <img src="https://via.placeholder.com/200" alt="" />
-            <p>NEW</p>
-            <h2>{title} Maker</h2>
+            <p className="tool-new">NEW</p>
+            <p className="tool-coming-soon">COMING SOON</p>
+            <h2><span className="tool-card-title">{title}</span> Maker</h2>
             <p>{body}</p>
             <a href="#" className="gradient-btn">Use</a>
         </div>
