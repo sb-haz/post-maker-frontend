094e63e88120ed5e5a991e6df435e8ad152eebc5
sb-haz <sb_haz@icloud.com>
2022-02-05 03:41:15 +0000

    add button with custom gradient

--- src/components/DemoVideo.js
+++ src/components/DemoVideo.js
@@ -2,7 +2,6 @@ const DemoVideo = () => {
     return (
         <div className="demo-video">
             <img src="https://via.placeholder.com/350" alt="" />
-            <blockquote class="twitter-tweet"><p lang="en" dir="ltr">I don&#39;t know what this goat is going through but I understand ❣️ <a href="https://t.co/XWHiNtmcXN">pic.twitter.com/XWHiNtmcXN</a></p>&mdash; The Igbo Wolf (@TheIgboWolf_) <a href="https://twitter.com/TheIgboWolf_/status/1489501217210384384?ref_src=twsrc%5Etfw">February 4, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>
         
     );
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,8 +6,9 @@ const HeroSection = () => {
             <div className="hero-info">
                 <h1>Create <span className="insta-colour-gradient">Instagram</span>-ready<br />videos from <span className="twitter-colour-gradient">tweets</span>.</h1>
                 <p>Make watermarked quotes & videos that are ready to upload to Instagram.
+                    <br />
                     Just copy & paste the tweet share link, and we'll email you the video.</p>
-                <a href="#">Start</a>
+                <a href="#" className="hero-start-btn">Start</a>
             </div>
             <div className="hero-video">
                 <DemoVideo />
