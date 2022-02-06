9f9f3a310659dd5ed58045529d48422b12cfbbfb
sb-haz <sb_haz@icloud.com>
2022-02-06 20:01:49 +0000

    improve sizing

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="footer">
             <div className="footer-text">
                 <h1 className="logo footer-logo">Post Maker</h1>
-                <p>© 2022 Post Maker — enquiries@post-maker.com</p>
+                <p><span>© 2022 Post Maker</span> <span>—</span> <span>enquiries@post-maker.com</span></p>
             </div>
             <img src="/icons/twitter-brands.svg" alt="" />
             {/* <img src="/icons/instagram-brands.svg" alt="" />*/}
