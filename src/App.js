34f3d76e3c72c7f9b7700767716f9a2dece9d327
sb-haz <sb_haz@icloud.com>
2022-02-06 04:50:39 +0000

    add responsiveness

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="content">
 
       <Navbar />
       <Home />
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ const HeroSection = () => {
     return (
         <div className="hero-section">
             <div className="hero-info">
-                <h1>Create <span className="insta-colour-gradient">Instagram</span>-ready<br />videos from <span className="twitter-colour-gradient">tweets</span>.</h1>
+                <h1 id="hero-title">Create <span id="hero-title" className="insta-colour-gradient">Instagram</span>-ready<br />videos from <span id="hero-title" className="twitter-colour-gradient">tweets</span>.</h1>
                 <p>Make watermarked quotes & videos that are ready to upload to Instagram.
                     <br />
                     Just copy & paste the tweet share link, and we'll email you the video.</p>
