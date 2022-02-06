ccde271039dfeb108bdfce6b1b3fb322ce5ac527
sb-haz <sb_haz@icloud.com>
2022-02-06 19:01:10 +0000

    fix footer mobile responsivness

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,8 @@ const Footer = () => {
                 <h1 className="logo footer-logo">Post Maker</h1>
                 <p>© 2022 Post Maker — app.postmaker@gmail.com</p>
             </div>
-            <div className="footer-socials">
-                <img src="/icons/twitter-brands.svg" alt="" />
-                {/* <img src="/icons/instagram-brands.svg" alt="" />*/}
-            </div>
+            <img src="/icons/twitter-brands.svg" alt="" />
+            {/* <img src="/icons/instagram-brands.svg" alt="" />*/}
         </div>
     );
 };
