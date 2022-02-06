b4830ec6e91bb471d1b809f72d516276613f2997
sb-haz <sb_haz@icloud.com>
2022-02-06 02:09:23 +0000

    netlify test

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="footer">
             <div className="footer-text">
                 <h1 className="logo footer-logo">Post Maker</h1>
-                <p>© 2022 Post Maker — app.postmaker@gmail.com</p>
+                <p>© 2022 Post Maker — app.postmaker@outlook.com</p>
             </div>
             <div className="footer-socials">
                 <img src="/icons/twitter-brands.svg" alt="" />
