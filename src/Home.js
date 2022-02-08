f98419db9823caf64433507ba9f6339dad2c7316
sb-haz <sb_haz@icloud.com>
2022-02-08 04:02:47 +0000

    add new animated divider

--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,15 @@ import Features from './components/Features';
 const Home = () => {
     return (
         <>
-            <div className="content hero-content">
+            <div className="content">
                 <HeroSection />
             </div>
-            
-            <div className="spacer waves-top full-width"></div>
 
-            <div className="post-spacer full-width"><AvailableTools /></div>
+            <div className="spacer divider-animated-waves"></div>
 
-            <div className="spacer waves-bottom full-width"></div>
+            <AvailableTools />
+
+            <div className="spacer divider-tilted-triange  "></div>
 
             <div className="content">
                 <Features />
