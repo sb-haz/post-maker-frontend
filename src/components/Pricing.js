9c980c099d6cad1ee1c34d13c6d4b5e76061bce1
sb-haz <sb_haz@icloud.com>
2022-02-08 23:46:55 +0000

    improve pricing table

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -5,43 +5,45 @@ const Pricing = () => {
         <div className="content pricing-section">
             <h1>Pricing</h1>
             <div className="pricing-table">
-                <table>
-                    <tr>
-                        <th></th>
-                        <th>Free</th>
-                        <th>Creator</th>
-                    </tr>
-                    <tr>
-                        <td>Monthly Price</td>
-                        <td>£0</td>
-                        <td className="blur-price">£19/m</td>
-                    </tr>
-                    <tr>
-                        <td>Videos</td>
-                        <td>3 per day</td>
-                        <td>✔</td>
-                    </tr>
-                    <tr>
-                        <td>Reels</td>
-                        <td>3 per day</td>
-                        <td>✔</td>
-                    </tr>
-                    <tr>
-                        <td>Quotes</td>
-                        <td>✔</td>
-                        <td>✔</td>
-                    </tr>
-                    <tr>
-                        <td>Watermark</td>
-                        <td>Default</td>
-                        <td>Customizable</td>
-                    </tr>
-                    <tr>
-                        <td>Render Speed</td>
-                        <td>Default</td>
-                        <td>Priority</td>
-                    </tr>
-                </table>
+                <div className="table-wrapper">
+                    <table>
+                        <tr>
+                            <th></th>
+                            <th>Free</th>
+                            <th>Creator</th>
+                        </tr>
+                        <tr>
+                            <td>Monthly Price</td>
+                            <td>£0</td>
+                            <td className="blur-price">£19/m</td>
+                        </tr>
+                        <tr>
+                            <td>Videos</td>
+                            <td>3 per day</td>
+                            <td>✔</td>
+                        </tr>
+                        <tr>
+                            <td>Reels</td>
+                            <td>3 per day</td>
+                            <td>✔</td>
+                        </tr>
+                        <tr>
+                            <td>Quotes</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                        </tr>
+                        <tr>
+                            <td>Watermark</td>
+                            <td>Default</td>
+                            <td>Customizable</td>
+                        </tr>
+                        <tr>
+                            <td>Render Speed</td>
+                            <td>Default</td>
+                            <td>Priority</td>
+                        </tr>
+                    </table>
+                </div>
 
                 <div className="trial-btn">
                     <Link to='/' className="gradient-btn">Start 30 Day Trial</Link>
