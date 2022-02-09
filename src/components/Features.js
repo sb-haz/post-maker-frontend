e78eda49e5fb96e52f8d3be7daaf0eba122aca5a
sb-haz <sb_haz@icloud.com>
2022-02-09 21:46:07 +0000

    remove feature info

--- src/components/Features.js
+++ src/components/Features.js
@@ -22,16 +22,6 @@ const Features = () => {
             title: 'Free Updates Forever',
             icon: '/icons/wrench-solid.svg',
             body: `New features are added regularly. Look out for watermark & caption customisation.`
-        },
-        {
-            title: 'Hassle-free Billing',
-            icon: '/icons/wrench-solid.svg',
-            body: `We utilise PayPal and Stripe payments which gives you the flexibility of paying how you want.`
-        },
-        {
-            title: '24/7 Support',
-            icon: '/icons/wrench-solid.svg',
-            body: `Got questions, concerns or suggestions? Contact us and we'll get back to you within 24 hours.`
         }]
 
     return (
@@ -42,8 +32,6 @@ const Features = () => {
                 <FeatureCard title={info[1].title} icon={info[1].icon} body={info[1].body} />
                 <FeatureCard title={info[2].title} icon={info[2].icon} body={info[2].body} />
                 <FeatureCard title={info[3].title} icon={info[3].icon} body={info[3].body} />
-                <FeatureCard title={info[4].title} icon={info[4].icon} body={info[4].body} />
-                <FeatureCard title={info[5].title} icon={info[5].icon} body={info[5].body} />
             </div>
         </div>
     );
