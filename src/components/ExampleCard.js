8e409617f3af0dc5c0c8dbc319ac55f54d740c97
sb-haz <sb_haz@icloud.com>
2022-02-09 00:37:29 +0000

    stop video autoplay

--- src/components/ExampleCard.js
+++ src/components/ExampleCard.js
@@ -2,7 +2,7 @@ const ExampleCard = ({ media_src }) => {
     return (
         <div className="example-card shadow">
             <h1 className="coloured-tag blue">Example</h1>
-            <video src={media_src} autoPlay muted playsinline loop >
+            <video src={media_src} controls playsinline loop >
                 <source src={media_src}
                     type="video/mp4" />
             </video>
