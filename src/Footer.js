3ed4969c9cf5bed4ef46ed47eb2841647c1c4e81
sb-haz <sb_haz@icloud.com>
2022-02-09 04:17:36 +0000

    add footer twitter link

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,16 @@ const Footer = () => {
     return (
         <div className="footer">
             <div className="footer-text">
-                <h1 className="logo footer-logo">Post Maker</h1>
-                <p><span>© 2022 Post Maker</span> <span>—</span> <span>enquiries@post-maker.com</span></p>
+                <h1 className="logo footer-logo unselectable">Post Maker</h1>
+                <p>
+                    <span className="unselectable">© 2022 Post Maker</span>
+                    <span className="unselectable"> — </span>
+                    <span>enquiries@post-maker.com</span>
+                </p>
             </div>
-            <img src="/icons/twitter-brands.svg" alt="" />
+            <a href="https://twitter.com/PostMakerApp" target="_blank" rel="noopener noreferrer">
+                <img src="/icons/twitter-brands.svg" alt="" />
+            </a>
         </div>
     );
 };
