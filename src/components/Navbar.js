56900270503a79c7e90fa706d18a6810196f21ac
sb-haz <sb_haz@icloud.com>
2022-02-09 23:29:36 +0000

    fix responsivness for large screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <navbar className="navbar">
+        <navbar className="content navbar">
             <NavLink to='/'><h1 className="logo insta-colour-gradient">Post Maker</h1></NavLink>
             <div className="links">
                 <NavLink className={(navData) => navData.isActive ? 'navbar-active' : 'navbar-link'}
