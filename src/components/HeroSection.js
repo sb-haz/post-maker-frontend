6f2ab6576befc532fd59f3ffab6b18606f4f0550
sb-haz <sb_haz@icloud.com>
2022-02-09 05:04:08 +0000

    update homepage title

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,10 +5,24 @@ const HeroSection = () => {
     return (
         <div className="hero-section">
             <div className="hero-info">
-                <h1 id="hero-title">Create <span id="hero-title" className="insta-colour-gradient">Instagram</span>-ready<br />videos from <span id="hero-title" className="twitter-colour-gradient">tweets</span>.</h1>
+                <h1 className="hero-title">
+                    <span>Create </span>
+                    <span className="insta-colour-gradient">
+                        Instagram
+                    </span>
+                    <span>-ready</span>
+                    <br />
+                    <span>content from </span>
+                    <span className="twitter-colour-gradient">
+                        Tweets
+                    </span>
+                    <span> & </span>
+                    <span className="tiktok-colour-gradient" >Tiktoks</span>
+                    <span>.</span>
+                </h1>
                 <p>Say good-bye to screenshotting, editing apps & downloading videos.
                     <br />
-                    <span id="hero-text-second-line">Let us handle the heavy work so you can focus on growing your page.</span>
+                    <span>Let us handle the heavy work so you can focus on growing your page.</span>
                 </p>
                 <a href="#link-available-tools" className="gradient-btn">Start</a>
             </div>
