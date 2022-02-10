f5b7ec7f7f0b2c975b154e23e5fdf4858ed5b76c
sb-haz <sb_haz@icloud.com>
2022-02-10 22:18:19 +0000

    temp enable autoplay

--- src/components/DemoVideo.js
+++ src/components/DemoVideo.js
@@ -1,7 +1,7 @@
 const DemoVideo = () => {
     return (
         <div className="hero-video">
-            <video width="350" height="350" muted playsinline loop >
+            <video width="350" height="350" autoPlay muted playsinline loop >
                 <source src="/examples/video_maker.mp4"
                     type="video/mp4" />
             </video>
