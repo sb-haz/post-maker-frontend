cd544ff126b1a54fdc60bbfd43ede9d6429a0d31
sb-haz <sb_haz@icloud.com>
2022-02-13 04:18:54 +0000

    use js map

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,7 +3,7 @@ import FeatureCard from "./FeatureCard";
 
 const Features = () => {
 
-    const info = [
+    const featuresInfo = [
         {
             title: 'Custom Watermarks',
             icon: '/icons/signature-solid.svg',
@@ -33,11 +33,11 @@ const Features = () => {
         <div className="features">
             <h1>Feature Packed</h1>
             <p>How does it work? <Link to="/guide" className="simple-btn">Learn more</Link></p>
+
             <div className="features-cards">
-                <FeatureCard title={info[0].title} icon={info[0].icon} body={info[0].body} />
-                <FeatureCard title={info[1].title} icon={info[1].icon} body={info[1].body} />
-                <FeatureCard title={info[2].title} icon={info[2].icon} body={info[2].body} />
-                <FeatureCard title={info[3].title} icon={info[3].icon} body={info[3].body} />
+                {featuresInfo.map(
+                    ({ title, icon, body, exmaple }) =>
+                        <FeatureCard title={title} icon={icon} body={body} />)}
             </div>
         </div>
     );
