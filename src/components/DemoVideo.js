721fa82280127d229cc3ab323d97f6d9c6a4f5af
sb-haz <sb_haz@icloud.com>
2022-02-13 04:45:42 +0000

    test safari playinline autoplay

--- src/components/DemoVideo.js
+++ src/components/DemoVideo.js
@@ -1,10 +1,13 @@
 const DemoVideo = () => {
     return (
         <div className="hero-video">
-            <video width="350" height="350" autoPlay loop muted playsinline>
+            <video src="/examples/video_maker.mp4" width="350" height="350"
+            playsinline loop muted autoPlay></video>
+
+            {/* <video width="350" height="350" autoPlay loop muted playsinline>
                 <source src="/examples/video_maker.mp4"
                     type="video/mp4" />
-            </video>
+            </video> */}
         </div>
 
     );
